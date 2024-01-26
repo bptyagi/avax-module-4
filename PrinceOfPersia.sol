b4b466a2214d36573c5e72c500c542da4587f946
bptyagi <155935978+bptyagi@users.noreply.github.com>
2024-01-26 10:40:50 +0530

    Update PrinceOfPersia.sol

--- PrinceOfPersia.sol
+++ PrinceOfPersia.sol
@@ -4,23 +4,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract PrinceOfPersia is ERC20, Ownable(msg.sender) {
 
-    uint256 public sands = 0;
-    uint256 public swordStrength = 1;
+    uint256 public sands ;
+    uint256 public swordStrength ;
 
     constructor() ERC20("PrinceOfPersia", "POP") {
         _mint(msg.sender, 10);
         sands = 10;
+        swordStrength = 1;
     }
 
     function collectSand(uint sandAmt) public onlyOwner {
         _mint(msg.sender, sandAmt);
+        sands += sandAmt;
     }
 
     function fightDemons() public {
 
         require(sands>=2 && swordStrength>0,"not enough strength to fight demons");
+        _burn(msg.sender,2);
         sands -= 2;
         swordStrength--;
     }
@@ -28,7 +32,7 @@ contract PrinceOfPersia is ERC20, Ownable(msg.sender) {
    function buySword() public {
         require(sands>=5,"not enough sand to forge sword");
         _burn(msg.sender, 5);
-
+        sands -= 5;
         swordStrength += 5;
     }
 
